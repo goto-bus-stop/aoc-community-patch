8c6a1c5fd42584d5fcc4e035ffa7a6709b4b87d6
Renée Kooi <renee@kooi.me>
2019-06-20 15:13:05 +0200

    -static-libstdc++ flag is a link option

--- src/features/brb.cpp
+++ src/features/brb.cpp
@@ -1,21 +1,20 @@
 #include "brb.h"
 #include "../auto_hook.h"
-#include <cstdio>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 
 static CallHook hook;
 
 int __cdecl read_hook(int32_t handle, char* buffer, uint32_t size) {
-  auto original = (int __cdecl (*) (int32_t, char*, uint32_t)) 0x614235;
+  auto original = (int __cdecl (*)(int32_t, char*, uint32_t))0x614235;
 
   auto result = original(handle, buffer, size);
-  // the game reserves lots and lots of space for this so we can safely add to the end
+  // the game reserves lots and lots of space for this so we can safely add to
+  // the end
   strcat(buffer, "\r\n\r\n\r\n\r\nbrb");
 
   return result;
 }
 
-void BRB::install() {
-  hook.install((void*) 0x529ED3, (void*) read_hook);
-}
+void BRB::install() { hook.install((void*)0x529ED3, (void*)read_hook); }
