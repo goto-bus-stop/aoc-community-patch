69cea49287195eb307b73a9d620a144425021752
Renée Kooi <renee@kooi.me>
2019-08-09 13:05:41 +0200

    rust style comments

--- src/features/brb.h
+++ src/features/brb.h
@@ -1,11 +1,8 @@
 #pragma once
 
-/**
- * Homage to UserPatch.
- *
- * Adds "brb" at the end of the credits.
- */
-
+/// Homage to UserPatch.
+///
+/// Adds "brb" at the end of the credits.
 class BRB {
 public:
   static void install();
--- src/features/mercenaries.h
+++ src/features/mercenaries.h
@@ -1,17 +1,14 @@
 #pragma once
 
-/**
- * Adds the necessary builtin features for the Cuman Mercenaries tech.
- *
- * Player attribute 214 contains the number of mercenaries that can be made.
- * Player attribute 215 contains the number of mercenaries that have been made.
- *
- * When 214 reaches 0, tech 708 is triggered.
- *
- * The data files can specify that 708 should disable the mercenary training
- * button for that player.
- */
-
+/// Adds the necessary builtin features for the Cuman Mercenaries tech.
+///
+/// Player attribute 214 contains the number of mercenaries that can be made.
+/// Player attribute 215 contains the number of mercenaries that have been made.
+///
+/// When 214 reaches 0, tech 708 is triggered.
+///
+/// The data files can specify that 708 should disable the mercenary training
+/// button for that player.
 class Mercenaries {
 public:
   static void install();
