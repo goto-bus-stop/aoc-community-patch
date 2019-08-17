811ad7ea84dce0b5de354ff38682e04c0beca086
Renée Kooi <renee@kooi.me>
2019-08-17 14:20:24 +0200

    remove debug stuff

--- src/features/scx_mod_identifier.cpp
+++ src/features/scx_mod_identifier.cpp
@@ -7,15 +7,10 @@ static CallHook on_save_scx_;
 static UserPatch up;
 
 static void THISCALL(on_save_hook, void* scx, void* handle) {
-  printf("on_save_hook(%p, %p)\n", scx, handle);
   auto save = getMethod<void, void*, void*>(0x533180);
   auto set_player_name = getMethod<void, void*, uint32_t, const char*>(0x532940);
-  auto get_player_name = getMethod<char*, void*, uint32_t>(0x5330F0);
 
-  printf("set_player_name_before(%p, %p, %s)\n", scx, handle, up.modShortName());
-  printf("%p\n", up.modShortName());
   set_player_name(scx, 9, up.modShortName());
-  printf("set_player_name_after(%p, %p, %s)\n", scx, handle, get_player_name(scx, 9));
   save(scx, handle);
 }
 
