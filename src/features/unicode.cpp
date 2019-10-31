16f6c95dea9569e9e4038f70ca748c596f385dfd
Renée Kooi <renee@kooi.me>
2019-10-31 11:52:05 +0100

    unicode: add DrawTextA implementation

--- src/features/unicode.cpp
+++ src/features/unicode.cpp
@@ -11,6 +11,12 @@ BOOL WINAPI utf8_text_out(HDC hdc, int x, int y, char* string, int c) {
   return 1;
 }
 
+BOOL WINAPI utf8_draw_text(HDC hdc, char* string, int c, RECT* rect, UINT format) {
+  auto len = MultiByteToWideChar(CP_UTF8, 0, string, c, temp_wide_str, sizeof(temp_wide_str));
+  DrawTextW(hdc, temp_wide_str, len, rect, format);
+  return 1;
+}
+
 static wchar_t* temp_str_ptr = nullptr;
 int WINAPI utf8_load_string(HINSTANCE hInstance, UINT uID, char* str_out, int cchBufferMax) {
   auto len = LoadStringW(hInstance, uID, reinterpret_cast<wchar_t*>(&temp_str_ptr), 0);
@@ -40,9 +46,9 @@ BOOL WINAPI utf8_text_metrics(HDC hdc, LPTEXTMETRICA lptm) {
   lptm->tmDigitizedAspectY = wide.tmDigitizedAspectY;
   lptm->tmDigitizedAspectX = wide.tmDigitizedAspectX;
   lptm->tmFirstChar = static_cast<char>(wide.tmFirstChar);
-  lptm->tmLastChar = static_cast<char>(wide.tmFirstChar);
-  lptm->tmDefaultChar = static_cast<char>(wide.tmFirstChar);
-  lptm->tmBreakChar = static_cast<char>(wide.tmFirstChar);
+  lptm->tmLastChar = static_cast<char>(wide.tmLastChar);
+  lptm->tmDefaultChar = static_cast<char>(wide.tmDefaultChar);
+  lptm->tmBreakChar = static_cast<char>(wide.tmBreakChar);
   lptm->tmItalic = wide.tmItalic;
   lptm->tmUnderlined = wide.tmUnderlined;
   lptm->tmStruckOut = wide.tmStruckOut;
@@ -58,11 +64,13 @@ BOOL WINAPI utf8_text_extent(HDC hdc, char* string, int c, LPSIZE psizl) {
 }
 
 static BytesHook text_out_hook_;
+static BytesHook draw_text_hook_;
 static BytesHook load_string_hook_;
 static BytesHook text_metrics_hook_;
 static BytesHook text_extent_hook_;
 
 static void* ptr_text_out_a;
+static void* ptr_draw_text_a;
 static void* ptr_load_string_a;
 static void* ptr_text_metrics_a;
 static void* ptr_text_extent_a;
@@ -74,6 +82,7 @@ void Unicode::install() {
   ptr_text_extent_a = (void*)utf8_text_extent;
 
   text_out_hook_.install((void*)0x635040, &ptr_text_out_a, sizeof(ptr_text_out_a));
+  draw_text_hook_.install((void*)0x6352EC, &ptr_draw_text_a, sizeof(ptr_draw_text_a));
   load_string_hook_.install((void*)0x635398, &ptr_load_string_a, sizeof(ptr_load_string_a));
   text_metrics_hook_.install((void*)0x635070, &ptr_text_metrics_a, sizeof(ptr_text_metrics_a));
   text_extent_hook_.install((void*)0x635078, &ptr_text_extent_a, sizeof(ptr_text_extent_a));
