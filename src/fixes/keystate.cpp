ead14a754e1c5b0d569988f5c6b70ec6ab2e3a61
Renée Kooi <renee@kooi.me>
2019-12-08 11:43:54 +0100

    Get rid of debug prints in keystate fix

--- src/fixes/keystate.cpp
+++ src/fixes/keystate.cpp
@@ -3,19 +3,12 @@
 #include <cstdint>
 #include <windows.h>
 
-#ifdef NDEBUG
-#define dbg_print(...)
-#else
-#define dbg_print(...) printf("[community-patch] [keystate] " __VA_ARGS__)
-#endif
-
 static BytesHook key_state_hook_;
 static BytesHook keyboard_state_hook_;
 static BytesHook async_key_state_hook_;
 
 uint16_t WINAPI get_key_state(int32_t virt_key) {
   auto state = static_cast<uint16_t>(GetKeyState(virt_key));
-  dbg_print("get_key_state(%d) = %x -> %x\n", virt_key, state, state & 0x8001);
   // Only the low-order and high-order bits are defined.
   return state & 0x8001;
 }
@@ -24,7 +17,6 @@ uint16_t WINAPI get_key_state(int32_t virt_key) {
 
 int32_t WINAPI get_keyboard_state(uint8_t* states) {
   auto res = GetKeyboardState(states);
-  dbg_print("keyboard_state: [< %x > %x ^ %x v %x]\n", states[VK_LEFT], states[VK_RIGHT], states[VK_UP], states[VK_DOWN]);
 
   for (auto i = 0; i < 256; i += 1) {
     // Only the low-order and high-order bits are defined.
@@ -36,8 +28,6 @@ int32_t WINAPI get_keyboard_state(uint8_t* states) {
 
 uint16_t WINAPI get_async_key_state(int32_t virt_key) {
   auto state = static_cast<uint16_t>(GetAsyncKeyState(virt_key));
-  dbg_print("get_async_key_state(%d) = %x -> %x\n", virt_key, state,
-            state & 0x8001);
   // Only the low-order and high-order bits are defined.
   return state & 0x8001;
 }
